0422b80790891cf983a75ba2648abe4bacf7681c
bea <chamoischamois@protonmail.com>
2022-02-22 22:19:02 +0800

    Fixed radio button option and loop function to display new book details

--- script.js
+++ script.js
@@ -7,22 +7,35 @@ function Book(title, author, pages, status) {
     this.status = status;
 }
 
-
 const form = document.querySelector("#new-book-form").addEventListener("submit", function(event){
     addBook(this);
     event.preventDefault();
 });
-
 
 function addBook(form) {
     let newBook = new Book(
         this.title.value,
         this.author.value,
         this.pages.value,
-        this.status.value
+        document.querySelector("input[type=radio]:checked").value
     );
 
-    console.log(newBook);
+    form.reset();
     library.push(newBook);
-    console.log(...library);
+    return addToShelf();
+      
+}
+
+const shelf = document.getElementById("shelf");
+
+function addToShelf() {
+library.forEach(book => {
+    const bookCard = document.createElement("div");
+    for (const detail in book) {
+    const divContent = document.createElement("div")
+    divContent.innerText = `${detail}: ${book[detail]}`;    
+    bookCard.appendChild(divContent);
+    }
+    shelf.appendChild(bookCard);
+});
 }
