ce05a6ca21c1db15c3591ec17649323176df2f28
bea <chamoischamois@protonmail.com>
2022-03-24 15:53:48 +0800

    Added form validation using JS

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ class Book {
     }
 
     static form = document.querySelector("#new-book-form").addEventListener("submit", function(event){
+        if ((/^\d+$/.test(pagesInput.value)) && (document.querySelector('input[name="status"]:checked') != null)) {
         addBook(this);
         event.preventDefault();
+        } else {
+            displayError();
+            event.preventDefault();
+        }
     });
 
     static shelf = document.getElementById("shelf");
@@ -96,3 +101,23 @@ const newBookButton = document.getElementById("new-book").addEventListener("clic
         formToggle.style.display = "none";
     }
 })
+
+
+const errorMessage = document.getElementById('error-message');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+
+function displayError() {
+    if (titleInput.validity.valueMissing) {
+        errorMessage.textContent = 'Please fill in the book title.';
+    } else if (authorInput.validity.valueMissing) {
+        errorMessage.textContent = 'Please fill in the author name.';
+    } else if (pagesInput.validity.valueMissing) {
+            errorMessage.textContent = 'Please fill in the number of pages.'
+    } else if (/^\d+$/.test(pagesInput.value) === false) {
+        errorMessage.textContent = 'Please fill in a valid number of pages.';
+    } else if (document.querySelector('input[name="status"]:checked') === null) {
+        errorMessage.textContent = 'Please select the reading status.'
+    }
+}
